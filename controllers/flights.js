4366a3e5927a76501e13a269765b620c9a6d8849
Colin MacDonald <colinmacdonald1213@yahoo.com>
2020-08-25 11:01:13 -0400

    update Show

--- controllers/flights.js
+++ controllers/flights.js
@@ -25,18 +25,10 @@ function newFlight(req, res) {
 }
 
 function create(req, res) {
-  req.body.nowShowing = !!req.body.nowShowing;
-  req.body.cast = req.body.cast.replace(/\s*,\s*/g, ',');
-  if (req.body.cast) req.body.cast = req.body.cast.split(',');
-  for (let key in req.body) {
-    if (req.body[key] === '') delete req.body[key];
-  }
   const flight = new Flight(req.body);
   flight.save(function(err) {
-    // one way to handle errors
     if (err) return res.redirect('/flights/new');
     console.log(flight);
-    // for now, redirect right back to new.ejs
-    res.redirect('/flights');
+    res.redirect(`/flights/${flight._id}`);
   });
 }
